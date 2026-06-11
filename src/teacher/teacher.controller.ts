31608368f3a8f5219e4ad72651e0b076357dbed4
MostafizurRahman199 <fardilshifat199@gmail.com>
2026-06-12 00:14:35 +0600

    implement CRUD operations for teacher module using Mongoose model

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+
+import { TeacherService } from './teacher.service';
+import { Teacher } from './teacher.schema';
 
 @Controller('teacher')
-export class TeacherController {}
+export class TeacherController {
+    constructor(private readonly teacherService:TeacherService){}
+
+    @Post()
+    async addTeacher(@Body() data:Partial<Teacher>){
+        return this.teacherService.createTeacher(data);
+    }
+
+    @Get()
+    async getAllTeacher(){
+        return this.teacherService.getAllTeacher();
+    }
+
+    @Get(':id')
+    async getTeacherById(
+        @Param('id') id:string
+    ){
+        return this.teacherService.getTeacherById(id);
+    }
+
+    @Put(':id')
+    async updateTeacher(
+        @Param('id') id:string,
+        @Body() data:Partial<Teacher>
+    ){
+        return this.teacherService.updateTeacher(id,data);
+    }
+
+}
